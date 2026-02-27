07ed453525ac0968f88d7abe4016adca91791cbd
RishawnP <r.paramapathy1@gmail.com>
2026-02-27 11:10:02 -0700

    requirements

---
+++ app/src/main/java/com/example/androiduitesting/ShowActivity.java
@@ -0,0 +1,36 @@
+package com.example.androiduitesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ShowActivity extends AppCompatActivity {
+    public static final String EXTRA_CITY_NAME = "com.example.androiduitesting.EXTRA_CITY_NAME";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        TextView cityNameView = findViewById(R.id.show_city_name);
+        Button backButton = findViewById(R.id.button_back);
+
+        Intent intent = getIntent();
+        String cityName = intent.getStringExtra(EXTRA_CITY_NAME);
+        if (cityName == null) {
+            cityName = "";
+        }
+        cityNameView.setText(cityName);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
